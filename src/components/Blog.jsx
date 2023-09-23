84e9f475b09368b0c72b789ef877f9cfee768dc3
unknown <dipjal5678@gmai.com>
2023-09-23 14:58:40 +0545

    [5.7] exercise, added show & hide button for more details of blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,3 +1,40 @@
-const Blog = ({ blog }) => <div>{blog.title}</div>;
+import { useState } from "react";
 
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleShowDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button onClick={toggleShowDetails}>
+          {showDetails ? "view" : "hide"}
+        </button>
+        {showDetails && (
+          <div>
+            <div>{blog.author}</div>
+            <div>{blog.url}</div>
+            <div>
+              likes {blog.likes}
+              <button>like</button>
+            </div>
+            <div>{blog.user.name}</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 export default Blog;
