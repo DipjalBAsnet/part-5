c96a39572561a404b0bd5c3d439b883d3bc2009e
unknown <dipjal5678@gmai.com>
2023-10-03 15:53:21 +0545

    [5.18] exercise, logged in successfull and wrong tested

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -7,4 +7,20 @@ describe("Blog app", function () {
   it("Login form is shown", function () {
     cy.contains("log in to application");
   });
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.contains("log in").click();
+      cy.get("#username").type("bikash");
+      cy.get("#password").type("gautam");
+      cy.get("#login-button").click();
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.contains("log in").click();
+      cy.get("#username").type("bikash");
+      cy.get("#password").type("bikash");
+      cy.get("#login-button").click();
+    });
+  });
 });
